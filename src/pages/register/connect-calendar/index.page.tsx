1db62282c97e36f8effce344dd48bbff48cef6f8
Alisson Macedo <alisson.macedo@alfreddelivery.com>
2023-05-20 22:38:35 -0300

    feat: add page connect-calendar

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,50 @@
+import { Button, Heading, MultiStep, Text, TextInput } from "@ignite-ui/react";
+import { Container, Header } from "../styles";
+import { ArrowRight } from "phosphor-react";
+import { z } from "zod";
+import { ConnectBox, ConnectItem } from "./styles";
+
+
+const registerFormSchema = z.object({
+  username: z.string()
+  .min(3, { message: 'O usuário precisa ter pelo menos 3 letras.' })
+  .regex(/^([a-z\\-]+)$/i, { message: 'O usuario pode ter apenas letras e hifens.'})
+  .transform((username) => username.toLocaleLowerCase()),
+  name: z.string().min(3, { message: 'O nome preicsa ter pelo menos 3 letras.'})
+})
+
+type RegisterFormData = z.infer<typeof registerFormSchema>
+
+export default function Register() {
+
+
+  async function handleRegister(data: RegisterFormData) {
+  }
+
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conect sua agenda!</Heading>
+        <Text>
+          Conecte seu calendario para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+        <Button type="submit">
+          Próximo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
