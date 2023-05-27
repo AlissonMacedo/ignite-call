ff4c9725d1a3ae3d4a3738417b73d1ee355754bc
Alisson Macedo <alisson.macedo@alfreddelivery.com>
2023-05-27 02:44:09 -0300

    feat: add days and hours of week

---
+++ src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,99 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsContainer,
+} from './styles'
+import { ArrowRight } from 'phosphor-react'
+import { useFieldArray, useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { getWeekDays } from '@/src/utils/get-week-days'
+
+const timeIntervalsFormSchema = z.object({})
+
+export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekday: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekday: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekday: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+
+  async function handleSetTimeIntervals() {}
+
+  const weekDays = getWeekDays()
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conect sua agenda!</Heading>
+        <Text>
+          Conecte seu calendario para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida que são agendados.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
+        <IntervalsContainer>
+          {fields.map((item, index) => {
+            return (
+              <IntervalItem key={item.id}>
+                <IntervalDay>
+                  <Checkbox />
+                  <Text>{weekDays[item.weekday]}</Text>
+                </IntervalDay>
+                <IntervalInputs>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.startTime`)}
+                  />
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.endTime`)}
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            )
+          })}
+        </IntervalsContainer>
+        <Button>
+          Proximo passo
+          <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
